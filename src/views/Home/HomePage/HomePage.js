245ad2b79f23bd263f35660a4916ab454be28c55
Julianne Adams <julianne@julianneadams.info>
2022-01-27 15:09:23 -0500

    Add: container for forum categories display

--- src/views/Home/HomePage/HomePage.js
+++ src/views/Home/HomePage/HomePage.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 import BoardCategoryList from "components/BoardCategoryList/BoardCategoryList";
+import ForumsDisplay from "views/Home/ForumsDisplay/ForumsDisplay";
 
 const HomePage = () => {
   return (
     <>
       <h1>Home</h1>
-      <BoardCategoryList />
+      <ForumsDisplay>
+        <BoardCategoryList />
+      </ForumsDisplay>
     </>
   );
 };
