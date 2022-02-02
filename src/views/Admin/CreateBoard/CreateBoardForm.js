3be2126d7978a8873ce13b1f8088ff8b4ae2708b
Julianne Adams <julianne@julianneadams.info>
2022-02-01 21:51:14 -0500

    Add: populate category list in board creation form

--- src/views/Admin/CreateBoard/CreateBoardForm.js
+++ src/views/Admin/CreateBoard/CreateBoardForm.js
@@ -1,5 +1,8 @@
+import React, { useEffect, useState } from "react";
+
 import {
   Button,
+  CircularProgress,
   FormControl,
   InputLabel,
   MenuItem,
@@ -7,7 +10,8 @@ import {
   TextField,
 } from "@mui/material";
 import { Box } from "@mui/system";
-import React, { useState } from "react";
+
+import useAxios from "hooks/useAxios";
 
 import * as S from "./CreateBoardForm.styles";
 
@@ -15,53 +19,83 @@ const CreateBoardForm = () => {
   const [topic, setTopic] = useState("");
   const [description, setDescription] = useState("");
   const [category, setCategory] = useState("");
+  const [categoryOptions, setCategoryOptions] = useState([]);
+
+  const { response, error, loading } = useAxios({
+    method: "GET",
+    url: "/boardCategories",
+  });
+
+  useEffect(() => {
+    if (response) {
+      setCategoryOptions(response.sort((a, b) => a.topic > b.topic));
+    }
+  }, [response]);
 
   return (
-    <S.Form autoComplete="off" data-testid="create-board-form">
-      <TextField
-        id="topic"
-        type="text"
-        label="Topic"
-        name="topic"
-        variant="outlined"
-        required
-        value={topic}
-        onChange={(e) => setTopic(e.target.value)}
-        inputProps={{ "data-testid": "topic", "aria-required": "true" }}
-      />
-      <TextField
-        id="description"
-        type="text"
-        label="Description"
-        name="description"
-        variant="outlined"
-        required
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-        inputProps={{ "data-testid": "description", "aria-required": "true" }}
-      />
-      <Box>
-        <FormControl fullWidth>
-          <InputLabel id="category-select-label">Category</InputLabel>
-          <Select
-            labelId="category-select-label"
-            id="category-select"
-            value={category}
-            label="Category"
-            name="category"
-            onChange={(e) => setCategory(e.target.value)}
-            inputProps={{ "data-testid": "category", "aria-required": "true" }}
-            required
-          >
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-          </Select>
-        </FormControl>
-      </Box>
-      <Button type="submit" variant="contained" sx={{ width: "20%" }}>
-        Submit
-      </Button>
-    </S.Form>
+    <>
+      {loading ? (
+        <CircularProgress />
+      ) : (
+        <Box>
+          {error && error.message}
+          <S.Form autoComplete="off" data-testid="create-board-form">
+            <TextField
+              id="topic"
+              type="text"
+              label="Topic"
+              name="topic"
+              variant="outlined"
+              required
+              value={topic}
+              onChange={(e) => setTopic(e.target.value)}
+              inputProps={{ "data-testid": "topic", "aria-required": "true" }}
+            />
+            <TextField
+              id="description"
+              type="text"
+              label="Description"
+              name="description"
+              variant="outlined"
+              required
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              inputProps={{
+                "data-testid": "description",
+                "aria-required": "true",
+              }}
+            />
+            <Box>
+              <FormControl fullWidth>
+                <InputLabel id="category-select-label">Category</InputLabel>
+                <Select
+                  labelId="category-select-label"
+                  id="category-select"
+                  value={category}
+                  label="Category"
+                  name="category"
+                  onChange={(e) => setCategory(e.target.value)}
+                  inputProps={{
+                    "data-testid": "category",
+                    "aria-required": "true",
+                  }}
+                  required
+                >
+                  {categoryOptions.map((option) => (
+                    <MenuItem value={option._id} key={option._id}>
+                      {option.topic}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Box>
+            <Button type="submit" variant="contained" sx={{ width: "20%" }}>
+              Submit
+            </Button>
+          </S.Form>
+        </Box>
+      )}
+    </>
   );
 };
 
